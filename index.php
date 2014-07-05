da4a83ae339f9429a618436f77dcb028171f8b18
CDEDrew <andre.jacques@cde-college.com>
2014-07-05 18:59:39 -0400

    Correct typo + head

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Lego Movie Rock's</title>
+	</head>
+
 	<body>
 		<h1>This is awesome</h1>
-		<p>Evrithing is awesome</p>`
+		<p>Evrithing is awesome</p>
 	</body>
 </html>
